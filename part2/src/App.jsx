1de8a00012ea940561db62ccb389777cb8e4a618
Laura Guo <lauraguo@proton.me>
2024-12-04 15:59:08 -0500

    Complete exercise 2.5 (separate Course into own module)

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map(part => <Part part={part} key={part.id} />)}
-  </>
-);
-
-const Course = ({ course }) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-);
-
-const Total = ({ parts }) => {
-  let total = parts.reduce((total, part) => total + part.exercises, 0);
-
-  return (
-    <>
-      <p style={{ fontWeight: 'bold' }}>Number of exercises {total}</p>
-    </>
-  )
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
