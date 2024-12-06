4659a142c8bbdabff0eda24801ae2b7cf5af9c99
Laura Guo <lauraguo@proton.me>
2024-12-05 22:24:12 -0500

    Complete exercise 2.7 (prevent duplicate names in phonebook)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  ]);
+  const [newName, setNewName] = useState('');
 
   const addPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
+
+    if (persons.some((person) => person.name === newName )) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     setPersons(persons.concat({ name: newName }));
     setNewName('');
   }
@@ -26,7 +32,7 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map((person) => <div key={person.name}>{person.name}</div>)}
     </div>
-  )
+  );
 }
 
 export default App
