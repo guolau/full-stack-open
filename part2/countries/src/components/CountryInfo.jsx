6248f53d953f60fb0035a3f8ea1291cb927de259
Laura Guo <lauraguo@proton.me>
2024-12-08 23:05:34 -0500

    Complete exercise 2.20 (add weather data to countries info)

--- part2/countries/src/components/CountryInfo.jsx
+++ part2/countries/src/components/CountryInfo.jsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+const weatherApiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
 const CountryInfo = ({ country }) => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat: country.capitalInfo.latlng[0],
+        lon: country.capitalInfo.latlng[1],
+        appid: weatherApiKey,
+        units: 'metric',
+      }
+    })
+    .then(response => {
+      setWeatherData(response.data);
+    })
+    .catch(error => {
+      alert('Unable to load weather data');
+    });
+  }, [])
+  
   return (
     <div>
       <h2>{country.name.common}</h2>
       <div>Capital: {country.capital[0]}</div>
       <div>Area: {country.area}</div>
       <div style={{marginTop: '1rem'}}>
-        <strong>Languages</strong>
+        <h3>Languages</h3>
         <ul>
           {Object.values(country.languages).map(
             language => <li key={language}>{language}</li>
@@ -15,6 +38,15 @@ const CountryInfo = ({ country }) => {
       <div style={{marginTop: '1rem'}}>
         <img src={country.flags.png}></img>
       </div>
+      {
+        weatherData &&
+        <div>
+          <h3>Weather in {country.capital[0]}</h3>
+            <div>Temperature: {weatherData.main.temp}°C</div>
+            <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}></img>
+            <div>Wind: {weatherData.wind.speed} m/s</div>
+        </div>
+      }
     </div>
   );
 }
