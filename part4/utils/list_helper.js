f8fa457c8634c5a45ed6e15ef6b55600a7418ca1
Laura Guo <lauraguo@proton.me>
2025-01-03 21:09:56 -0500

    Complete exercise 4.5 (unit tests for favoriteBlog helper)

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((currentFavoriteBlog, blog) => {
+    return currentFavoriteBlog.likes > blog.likes ? currentFavoriteBlog : blog
+  }, blogs[0])
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
