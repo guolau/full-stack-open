aae2eb40064603bc9b2e2c7cd11daab2ec3b4232
Laura Guo <lauraguo@proton.me>
2025-01-05 13:48:16 -0500

    Complete exercise 4.10 (test create blog)

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -3,7 +3,7 @@ const assert = require("node:assert")
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
-const { blogs } = require("./test_helper")
+const { blog, blogs } = require("./test_helper")
 const Blog = require("../models/blogs")
 
 const api = supertest(app)
@@ -14,13 +14,14 @@ beforeEach(async () => {
   await Promise.all(promises)
 })
 
-describe("/api/blogs", () => {
+describe("list blogs", () => {
   test("responds with JSON", async () => {
     await api
       .get("/api/blogs")
       .expect(200)
       .expect("Content-Type", /application\/json/)
   })
+
   test("responds with correct number of blogs", async () => {
     const response = await api.get("/api/blogs")
     assert.strictEqual(response.body.length, blogs.length)
@@ -31,6 +32,26 @@ test("id key is named 'id'", async () => {
   const response = await api.get("/api/blogs")
   const blog = response.body[0]
   assert(blog.hasOwnProperty("id"))
+})
+
+describe("create blog", () => {
+  test("increases total blogs by 1", async () => {
+    await api
+      .post("/api/blogs")
+      .send(blog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/)
+
+    const listResponse = await api.get("/api/blogs")
+
+    assert.strictEqual(blogs.length + 1, listResponse.body.length)
+  })
+
+  test("created with correct info", async () => {
+    const response = await api.post("/api/blogs").send(blog)
+    const blogInDb = await Blog.findOne({ _id: response.body.id })
+    assert.deepStrictEqual(new Blog(blog).toJSON(), blogInDb.toJSON())
+  })
 })
 
 after(async () => {
